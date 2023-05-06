0681eb3e0a723bdf0448cd0a1f8e5dcf246f060a
System Administrator <root@Hirushas-MacBook-Pro.local>
2023-05-06 09:55:15 -0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+//INTRNAL IMPORT
+import { NavBar, Footer } from "../components/componentsindex";
+import { NFTMarketplaceProvider } from "../Context/NFTMarketplaceContext";
+
+const MyApp = ({ Component, pageProps }) => (
+  <div>
+    <NFTMarketplaceProvider>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </NFTMarketplaceProvider>
+  </div>
+);
+
+export default MyApp;
